4921907a3120d0bd26f2331a156752cb09b5944f
liuwanwei <liuwanwei@gmail.com>
2018-09-30 16:02:19 +0800

    recommit

--- BDBaseKit.podspec
+++ BDBaseKit.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'BDBaseKit'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of BDBaseKit.'
+  s.summary          = '一些基础库代码，只言片语.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,14 +18,14 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+基础工具类，合并到一起便于使用和管理。
                        DESC
 
-  s.homepage         = 'https://github.com/liuwanwei@gmail.com/BDBaseKit'
+  s.homepage         = 'https://github.com/liuwanwei/BDBaseKit'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'liuwanwei@gmail.com' => 'liuwanwei@gmail.com' }
-  s.source           = { :git => 'https://github.com/liuwanwei@gmail.com/BDBaseKit.git', :tag => s.version.to_s }
+  s.source           = { :git => 'https://github.com/liuwanwei/BDBaseKit.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
   s.ios.deployment_target = '8.0'
@@ -39,4 +39,8 @@ TODO: Add long description of the pod here.
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.dependency 'AutoCoding'
+  s.dependency 'SDWebImage'
+  s.dependency 'MBProgressHUD'
+  s.dependency 'YSASIHTTPRequest'
 end
---
+++ BDBaseKit/Classes/BDUtils.h
@@ -0,0 +1,64 @@
+//
+//  Utils.h
+//  investigation
+//
+//  Created by sungeo on 14/12/4.
+//  Copyright (c) 2014年 buddysoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class UITableView;
+@class UIViewController;
+@class UIWebView;
+@class UIImage;
+
+@interface BDUtils : NSObject
+
+// 从Main Storyboard中加载一个UIViewController
++ (id)viewControllerWithIdentifier:(NSString *)identifier;
++ (id)viewControllerwithStoryboard:(NSString *)storyboard withIdentifier:(NSString *)identifier;
+
+// 展示一个UIAlertView
++ (void)warningWithMessage:(NSString *)message;
++ (void)warningWithTitle:(NSString *)title message:(NSString *)message;
++ (void)errorWithMessage:(NSString *)message;
+
++ (void)hideExtraCellsForTableView:(UITableView *)tableView;
+
+// 自定义导航栏返回按钮的文字
++ (void)customizeBackNavigationItemTitle:(NSString *)title forViewController:(UIViewController *)vc;
+
+/*
+ * 将秒数转换为"XX小时XX分XX秒"这样的形式
+ */
++ (NSString *)readableTimeFromSeconds:(NSTimeInterval)interval;
+
++ (void)customizeStatusBarForApplication:(id)application;
++ (void)customizeNavigationBarForApplication:(id)application withColor:(id)color;
++ (void)customizeNavigationBarForApplication:(id)application withImage:(UIImage *)image;
+
+/**
+ *  加载本地Html页面，文件在打包在App内部
+ *
+ *  @param fileName 文件名
+ *  @param webView  目标 WebView
+ */
++ (void)loadHtml:(NSString *)fileName webView:(UIWebView *)webView;
+
+/**
+ *  加载本地html页面，图片资源使用本地App内部资源
+ *
+ *  @param filePath 文件路径
+ *  @param webView  目标 WebView
+ */
++ (void)loadHtmlWithFilePath:(NSString *)filePath webView:(UIWebView *)webView;
+
+/**
+ *  加载bundle Json文件
+ *
+ *  @param fileName 文件名
+ */
++ (NSDictionary *)loadJsonFileFromBundel:(NSString *)fileName;
+
+@end
